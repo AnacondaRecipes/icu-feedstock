cbef7bf63723689eed4f5d1b021f29a1dad3b62b
Eric Lundby <Eric.Lundby@gmail.com>
2026-02-17 20:20:54 -0700

    fix pkg-config packages

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -62,3 +62,47 @@ if errorlevel 1 exit 1
 for %%t in (derb genbrk gencfu gencnval gendict gennorm2 genrb gensprep icuexportdata icuinfo makeconv pkgdata uconv) do (
     if exist "%BINDIR%\%%t.exe" copy /Y "%BINDIR%\%%t.exe" "%LIBRARY_BIN%\"
 )
+
+:: Install pkg-config files
+if not exist "%LIBRARY_LIB%\pkgconfig" mkdir "%LIBRARY_LIB%\pkgconfig"
+
+> "%LIBRARY_LIB%\pkgconfig\icu-uc.pc" (
+    echo prefix=${pcfiledir}/../..
+    echo exec_prefix=${prefix}
+    echo libdir=${exec_prefix}/lib
+    echo includedir=${prefix}/include
+    echo.
+    echo Name: icu-uc
+    echo Description: International Components for Unicode: Common Library
+    echo Version: 73.1
+    echo Libs: -L${libdir} -licuuc -licudt
+    echo Cflags: -I${includedir}
+)
+
+> "%LIBRARY_LIB%\pkgconfig\icu-i18n.pc" (
+    echo prefix=${pcfiledir}/../..
+    echo exec_prefix=${prefix}
+    echo libdir=${exec_prefix}/lib
+    echo includedir=${prefix}/include
+    echo.
+    echo Name: icu-i18n
+    echo Description: International Components for Unicode: Internationalization Library
+    echo Version: 73.1
+    echo Requires: icu-uc
+    echo Libs: -L${libdir} -licuin
+    echo Cflags: -I${includedir}
+)
+
+> "%LIBRARY_LIB%\pkgconfig\icu-io.pc" (
+    echo prefix=${pcfiledir}/../..
+    echo exec_prefix=${prefix}
+    echo libdir=${exec_prefix}/lib
+    echo includedir=${prefix}/include
+    echo.
+    echo Name: icu-io
+    echo Description: International Components for Unicode: Stream and I/O Library
+    echo Version: 73.1
+    echo Requires: icu-uc icu-i18n
+    echo Libs: -L${libdir} -licuio
+    echo Cflags: -I${includedir}
+)
