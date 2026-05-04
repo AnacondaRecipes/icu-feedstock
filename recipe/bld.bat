96763e9c410997f91668e4dd9ca3cb15721cede7
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-05-04 19:03:29 +0300

    icu-78.3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,41 +1,35 @@
 SETLOCAL EnableDelayedExpansion
 cd source
 
-:: Remove all instances of /W4 from configure.
-:: They get passed to the linker and confuse it.
-:: Given that we aren't interested in warnings anyway
-:: remove it. I could have used a patch but this seemed
-:: more 'upgrade friendly'
+:: Remove all instances of /W4 from configure -- they get passed to the linker
+:: which then dies on the unrecognised flag. We don't care about warnings here.
 sed "s~ /W4~~g" configure > configure.new
 move configure.new configure
 
-:: This seems to be required - not sure why but
-:: rc.exe gets confused with the '/' form of slashes
+:: rc.exe (Windows resource compiler) gets confused by the '/' form of slashes
+:: that MSYS2 substitutes; this env var keeps backslashes intact.
 set MSYS_RC_MODE=1
 
-:: 32-bit and VS2015 ends in failure:
-:: uconv.o : MSIL .netmodule or module compiled with /GL found; restarting link with /LTCG; add /LTCG to the link command line to improve linker performance
-:: uconv.o : error LNK2001: unresolved external symbol _uconvmsg_dat
-:: ../../bin/uconv.exe : fatal error LNK1120: 1 unresolved externals
-:: .. without this
-if "%ARCH%"=="32" (
-  if "%c_compiler%"=="vs2015" (
-    set EXTRA_OPTS=--disable-extras
-  )
-)
-
-:: This directory is needed during the install process but isn't created by the scripts.
+:: Needed during `make install` but ICU's tree doesn't pre-create it.
 mkdir data\out\tmp
 
-set BUILD=x86_64-pc-cygwin
-set HOST=x86_64-pc-cygwin
+echo "build - %build_platform% - %BUILD_PLATFORM%"
 cd ..
 
-copy "%RECIPE_DIR%\build.sh" .
+:: msys2 bash inherits the parent process env, but %build_platform% is a
+:: conda-build template variable that isn't auto-exported. Prepend it to a
+:: generated wrapper script so build.sh can branch on target/build platform.
+echo export build_platform=%build_platform% > build.sh
+type "%RECIPE_DIR%\build.sh" >> build.sh
+
 set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
 FOR /F "delims=" %%i in ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "PREFIX=%%i"
+:: ICU 78.x configure invokes Python during data/rules.mk generation. Without
+:: this override it tries to use _h_env_placehold_/.../python which doesn't
+:: exist at configure time. Point it at the real python.exe in BUILD_PREFIX.
+FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "ac_cv_prog_PYTHON=%%i/python.exe"
 
 set CC=cl.exe
 set CXX=cl.exe
@@ -43,6 +37,6 @@ set CXX=cl.exe
 bash -lc "./build.sh"
 if errorlevel 1 exit 1
 
-:: The .dlls end up in the wrong place
+:: `make install` deposits .dlls into lib/, but conda expects shared libs in bin/.
 move %LIBRARY_LIB%\icu*.dll %LIBRARY_BIN%
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,31 +2,36 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./source
 
-set -e
+set -ex
+
+if [[ "${target_platform:-}" == win-* ]]; then
+  # MSVC's link.exe must come before MSYS2's coreutils `link` on PATH.
+  export PATH="$(dirname "$(which -a link | grep MSVC | head -1)"):$PATH"
+  export CXXFLAGS="${CXXFLAGS:-} /std:c++17"
+  # Tell ICU's configure we're targeting MSVC (not real MSYS).
+  cp source/config/mh-msys-msvc source/config/mh-unknown
+  # configure mis-detects EXEEXT="" for *-pc-windows; force .exe back in.
+  sed -i.bak 's/EXEEXT=""/EXEEXT=.exe/g' source/configure
+  export BUILD=x86_64-pc-windows
+  export HOST=x86_64-pc-windows
+fi
 
 cd source
 
 chmod +x configure install-sh
 
-EXTRA_OPTS=""
+# conda-build does not export HOST in native builds and clang_bootstrap has
+# no activation script that would set it, so derive it from BUILD.
+HOST="${HOST:-${BUILD}}"
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-    mkdir cross_build
-    pushd cross_build
-    CC=$CC_FOR_BUILD CXX=$CXX_FOR_BUILD AR= AS= LD= CFLAGS= CXXFLAGS= LDFLAGS= CPPFLAGS= ../configure \
-      --build=${BUILD} \
-      --host=${BUILD} \
-      --disable-samples \
-      --disable-extras \
-      --disable-layout \
-      --disable-tests
-    make -j${CPU_COUNT}
-    EXTRA_OPTS="$EXTRA_OPTS --with-cross-build=$PWD"
-    popd
-fi
-
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  EXTRA_OPTS="$EXTRA_OPTS --enable-rpath"
+if [[ "${target_platform:-}" == osx-* ]]; then
+  # clang_bootstrap (used on macOS to keep libxml2/icu out of the build env)
+  # does not ship the activation scripts that normally inject
+  # -headerpad_max_install_names. ICU's short @rpath/libicu*.dylib install
+  # names mean even that flag would only reserve ~40 bytes -- not enough for
+  # conda-build to splice in an extra LC_RPATH afterwards. Reserve a fixed
+  # 4 KB pad in every Mach-O we link.
+  export LDFLAGS="${LDFLAGS:-} -Wl,-headerpad,0x1000"
 fi
 
 ./configure --prefix="${PREFIX}"  \
@@ -36,13 +41,11 @@ fi
             --disable-extras      \
             --disable-layout      \
             --disable-tests       \
-            --enable-static       \
-            ${EXTRA_OPTS}
+            --enable-static
 
 make -j${CPU_COUNT} ${VERBOSE_CM}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-  make check
-fi
+make check
 make install
 
+# Remove system utils need only for rebuild custom icu data
 rm -rf ${PREFIX}/sbin
